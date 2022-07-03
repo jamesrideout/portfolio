91476c87c72c14fc2e4b8bf8fd528c9d774c04e8
James Rideout <104699491+jamesrideout@users.noreply.github.com>
2022-07-04 00:31:52 +0100

    quotes object added

---
+++ resources/javascript/quotes.js
@@ -0,0 +1,79 @@
+const quotes = {
+    quote1: {
+        text: 'A good programmer is someone who always looks both ways before crossing a one-way street',
+        author: 'Doug Linder',
+        isCodingQuote: true
+    },
+    quote2: {
+        text: 'Functions should do one thing. They should do it well. They should do it only',
+        author: 'Robert C. Martin',
+        isCodingQuote: true
+    },
+    quote3: {
+        text: 'The fool wonders, the wise man asks',
+        author: 'Benjamin Disraeli',
+        isCodingQuote: false
+    },
+    quote4: {
+        text: 'In Theory There Is No Difference Between Theory and Practice, While In Practice There Is',
+        author: 'Jan L. A. van de Snepscheut',
+        isCodingQuote: true
+    },
+    quote5: {
+        text: 'Eighty percent of success is showing up',
+        author: 'Woody Allen',
+        isCodingQuote: false
+    },
+    quote6: {
+        text: 'Perserverance and spirit have done wonders in all ages',
+        author: 'George Washington',
+        isCodingQuote: false
+    },
+    quote7: {
+        text: 'Self-education is, I firmly believe, the only kind of education there is',
+        author: 'Isaac Asimov',
+        isCodingQuote: false
+    },
+    quote8: {
+        text: 'There are only two kinds of languages: the ones people complain about and the ones nobody uses',
+        author: 'Bjarne Stroustrup',
+        isCodingQuote: true
+    },
+    quote9: {
+        text: 'Good design adds value faster than it adds cost',
+        author: 'Thomas C. Gale',
+        isCodingQuote: false
+    },
+    quote10: {
+        text: 'It\'s all talk until the code runs',
+        author: 'Ward Cunningham',
+        isCodingQuote: true
+    },
+    quote11: {
+        text: 'Always code as if the guy who ends up maintaining your code will be a violent psychopath who knows where you live',
+        author: 'John Woods',
+        isCodingQuote: true
+    },
+    quote12: {
+        text: 'You miss 100 percent of the shots you never take',
+        author: 'Wayne Gretzky',
+        isCodingQuote: false
+    },
+    quote13: {
+        text: 'The good thing about science is that it\'s true whether or not you believe in it',
+        author: 'Neil deGrasse Tyson',
+        isCodingQuote: false
+    },
+    quote14: {
+        text: 'You cannot teach a man anything; you can only help him discover it in himself',
+        author: 'Galileo',
+        isCodingQuote: false
+    }
+};
+
+console.log(quotes.quote13.author);
+
+
+
+
+
